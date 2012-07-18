6611ae9042fe0e48153581e31b834fe11329be09
Rubén Norte <rubennorte@gmail.com>
2012-07-18 18:21:01 +0200

    Moved some functions from skeleton router to a new module which adds some methods to Backbone.history

---
+++ history.js
@@ -0,0 +1,66 @@
+
+/*!
+ * Skeleton.js - History
+ * Copyright(c) 2012 Rubén Norte <rubennorte@gmail.com>
+ * MIT Licensed
+ */
+
+define([
+  'backbone',
+  'config'
+], function(Backbone, config){
+
+  // Create the history object if it's not created, as Backbone does
+  Backbone.history || (Backbone.history = new Backbone.History);
+
+  _.extend(Backbone.history, {
+
+    /**
+     * Start method redefined to add some default values
+     */
+    start: function(options){
+      // Set default options
+      options || (options = {});
+      if (typeof(options.root) == 'undefined')
+        options.root = config.url.root;
+      if (typeof(options.pushState) == 'undefined')
+        options.pushState = true;
+
+      Backbone.History.prototype.start.call(this, options);
+    },
+
+    /**
+     * Generates a URL replacing the route params with the values of those
+     * params in the specified object
+     */
+    generateRoute: function(route, params){
+      return route.replace(/:(\w+)/g, function(match, paramName){
+        return params[paramName];
+      });
+    },
+
+    /**
+     * Returns the current route
+     */
+    getCurrentRoute: function(){
+      var current = Backbone.history.fragment,
+          root = Backbone.history.options.root;
+      // Fixes error in backbone
+      if (root && current.indexOf(root) == 0){
+        current = current.substr(root.length);
+      }
+      return current;
+    },
+
+    /**
+     * Triggers again the handler for the current route
+     */
+    refresh: function(){
+      this.loadUrl(this.getCurrentRoute());
+    }
+
+  });
+
+  return Backbone.history;
+  
+});
--- router.js
+++ router.js
@@ -15,46 +15,7 @@ define([
    */
   var Router = Backbone.Router.extend({
 
-    start: function(options){
-      // Set default options
-      options || (options = {});
-      if (typeof(options.root) == 'undefined')
-        options.root = config.url.root;
-      if (typeof(options.pushState) == 'undefined')
-        options.pushState = true;
-
-      Backbone.history.start(options);
-    },
-
-    /**
-     * Generates a URL replacing the route params with the values of those
-     * params in the specified object
-     */
-    generateRoute: function(route, params){
-      return route.replace(/:(\w+)/g, function(match, paramName){
-        return params[paramName];
-      });
-    },
-
-    /**
-     * Returns the current route
-     */
-    getCurrentRoute: function(){
-      var current = Backbone.history.fragment,
-          root = Backbone.history.options.root;
-      // Fixes error in backbone
-      if (root && current.indexOf(root) == 0){
-        current = current.substr(root.length);
-      }
-      return current;
-    },
-
-    /**
-     * Triggers again the handler for the current route
-     */
-    refresh: function(){
-      Backbone.history.loadUrl(this.getCurrentRoute());
-    }
+    // TODO Add skeleton specific router stuff
 
   });
 
